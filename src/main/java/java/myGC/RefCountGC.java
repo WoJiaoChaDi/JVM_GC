1db4112cbce61c05641cc108d50d484c45c869ef
wojiaochadi <264835229@qq.com>
2020-06-15 09:11:42 +0800

    JMM 引用计数

---
+++ src/main/java/java/myGC/RefCountGC.java
@@ -0,0 +1,19 @@
+package java.myGC;
+
+public class RefCountGC {
+    private byte[] bigSize = new byte[2 * 1024 * 1024];//这个成员属性唯一的作用就是占用一点内存
+    Object instance = null;
+
+    public static void main(String[] args) {
+        RefCountGC objectA = new RefCountGC();
+        RefCountGC objectB = new RefCountGC();
+        objectA.instance = objectB;
+        objectB.instance = objectA;
+        objectA = null;
+        objectB = null;
+
+        System.out.println("手动清除GC");
+        //手动执行GC
+        System.gc();
+    }
+}
